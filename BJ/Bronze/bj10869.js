1ee9394c5b5d3e926a983fce8f5d43b4500520e9
hge2ne <hge2ne@gmail.com>
2025-10-15 11:08:34 +0900

    bj10869.js

--- BJ/Bronze/bj10869.js
+++ BJ/Bronze/bj10869.js
@@ -6,5 +6,8 @@ const [A, B] = fs.readFileSync(0, "utf-8").trim().split(/\s+/).map(Number);
 console.log(A + B);
 console.log(A - B);
 console.log(A * B);
-console.log(A / B);
+console.log(Math.floor(A / B));
+/* 
+Math.floor() : 소수점 내림(다버림)
+*/
 console.log(A % B);
